bedf537206bfd8e81dca6dfcd0ba6f295157772f
exbarlow <exbarlow@gmail.com>
2022-03-09 14:42:23 -0600

    boo it doesnt work

--- popup.js
+++ popup.js
@@ -1,3 +1,20 @@
+// Settings, duration of each timer mode type
+let settings = {
+    focus: 1,
+    rest: 5,
+}
+
+// Timer object, contains information about current mode and remaining time in current mode
+let timer = {
+    mode: 'focus',
+    remainingTime: {
+        total: settings['focus'] * 60,
+        minutes: settings['focus'],
+        seconds: 0,
+    }
+}
+
+let interval;
 
 // get control buttons
 const startButton = document.querySelector('#startButton');
@@ -8,13 +25,53 @@ const quitButton = document.querySelector('#quitButton');
 const countdownDisplay = document.querySelector('.countdown');
 const modeDisplay = document.querySelector('.mode');
 
-// when the popup is open, want to get the remainingTime of timer
-chrome.runtime.sendMessage('get-time', (response) => {
-    updateCountdownText(response);
-})
 
+// Returns the remaining time in the timer given the endTime
+function getRemainingTime() {
+    let endTime;
+    chrome.storage.local.get('timerEnd', function (result) {
+        endTime = result.timerEnd;
+        alert(endTime);
+        const difference = endTime - Date.now();
+        // alert(difference)
+        const total = Math.floor(difference / 1000);
+        const minutes = Math.floor((total / 60) % 60); // I think the modulo can probably be removed once
+        const seconds = Math.floor(total % 60);
+
+        return {
+            total,
+            minutes,
+            seconds,
+        };
+    });
+
+}
+
+
+function startTimer() {
+    let total = timer.remainingTime.total;
+    const endTime = Date.now() + total * 1000;
+    chrome.storage.local.set({ timerEnd: endTime })
+    runTimer();
+}
+
+
+
+function runTimer() {
+    interval = window.setInterval(() => {
+        total = timer.remainingTime.total;
+        if (total <= 0) {
+            window.clearInterval(interval);
+        }
+        timer.remainingTime = getRemainingTime();
+        updateCountdownText(timer.remainingTime);
+    }, 1000);
+}
+
+// actually updates the time on the countdown timer
 function updateCountdownText(rT) {
-    const remainingTime = rT;
+
+    const remainingTime = rT
     const minutes = `${remainingTime.minutes}`.padStart(2, '0');
     const seconds = `${remainingTime.seconds}`.padStart(2, '0');
 
@@ -22,21 +79,9 @@ function updateCountdownText(rT) {
     countdownDisplay.innerText = newText;
 }
 
-startButton.addEventListener("click", () => {
-    console.log("Clicked!");
+startButton.addEventListener('click', () => {
     startTimer();
 })
 
-
-// switches to the given mode, resetting the remainingTime
-function switchMode(mode) {
-    timer.mode = mode;
-    timer.remainingTime.total = settings[mode] * 60;
-    timer.remainingTime.minutes = settings[mode];
-    timer.remainingTime.seconds = 0;
-
-    updateCountdown();
-    modeDisplay.innerText = `${mode}`;
-}
 
 
